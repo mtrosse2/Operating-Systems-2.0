0989dd53a5948fd70e7b5444c9b3653c3df9ac94
Mitchell Trossen <mtrosse2@nd.edu>
2023-04-12 23:20:50 -0400

    Probs Done Hash

--- Project04/simple-hash.c
+++ Project04/simple-hash.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-
+#include <stdint.h>
 #include "pcap-process.h"
 #include "pcap-process.c"
 #include "packet.h"
@@ -15,6 +15,9 @@ struct hash_table {
 };
 
 // https://en.wikipedia.org/wiki/Jenkins_hash_function
+
+
+/*
 uint32_t jenkins(const uint8_t* key, size_t length) {
     size_t i = 0;
     uint32_t hash = 0;
@@ -28,14 +31,29 @@ uint32_t jenkins(const uint8_t* key, size_t length) {
     hash += hash << 15;
     return hash;
 }
+*/
+
+uint32_t jenkins_16bit(uint16_t key) {
+    uint32_t hash = 0;
+    hash += key;
+    hash += hash << 10;
+    hash ^= hash >> 6;
+    hash += hash << 3;
+    hash ^= hash >> 11;
+    hash += hash << 15;
+    return hash;
+}
+
+
+
 
 int hashData(int netPayload) {
-    int hash_value = jenkins(netPayload, sizeof(netPayload));
+    int hash_value = jenkins_16bit(netPayload);
     // get hash_value from jenkins hash using PacketEntry->ThePacket->Data + PacketEntry->ThePacket->PayloadOffset (ie. netPayload)
     return hash_value % TABLE_SIZE;
 }
 
-int getNetPayload(struct PacketEntry * pEntry) {
+int getNetPayload(struct PacketEntry * pEntry){
     return pEntry->ThePacket->LengthIncluded - pEntry->ThePacket->PayloadOffset;
 }
 
@@ -56,31 +74,28 @@ void insert(struct hash_table * ht, struct PacketEntry * pEntry ) {
         ht->table[index] = new_node;
     }
     else if (sizeof(existing->key) == sizeof(getNetPayload(pEntry))) {
-        if (memcpy(existing->key, getNetPayload(pEntry), sizeof(existing->key) == 0)) {
+        if (memcmp((void*) existing->key, (void *) getNetPayload(pEntry), sizeof(existing->key) == 0)) {
             // this means the netPayloads are the same
             // this is a hit
             existing->HitCount++;
             existing->RedundantBytes += sizeof(getNetPayload(pEntry));
 
             // discard the one being inserted
-            discard(new_node);
+            discardPacket(new_node->ThePacket);
         }
     }
     else {
         // if existing hit count is greater than < 1 evict the exising packetentry
         if (existing->HitCount < 1) {
             // discard existing
-            discard(existing);
+            discardPacket(existing->ThePacket);
             
             ht->table[index] = new_node;
         }
         else {
             // discard the new one
-            discard(new_node);
+            discardPacket(new_node->ThePacket);
         }
     }
 }
 
-int main (void){
-    return 0;
-}
