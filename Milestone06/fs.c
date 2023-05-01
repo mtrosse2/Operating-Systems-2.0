bdfa20424d541f1a41683b4a7e725a70999e261b
Mitchell Trossen <mtrosse2@nd.edu>
2023-04-30 23:32:32 -0400

    round one

--- Milestone06/fs.c
+++ Milestone06/fs.c
@@ -11,10 +11,96 @@ Make your changes here.
 #include <string.h>
 
 extern struct disk *thedisk;
+int *free_blocks[2000] = {1}; // zero is false, anything else true
+int *free_inodes; 
+// ************************************
+int  fs_load_inode( size_t inumber, fs_inode *node);
+int fs_save_inode( size_t inumber, fs_inode *node);
+int search_free_list();
+
+
+int fs_load_inode( size_t inumber, Inode *node){
+		// find blk and offset val
+    ssize_t iBlock = (ssize_t) ((inumber + INODES_PER_BLOCK - 1)/ INODES_PER_BLOCK);
+
+    if (iBlock == 0){
+        iBlock = 1;
+    }
+    
+    ssize_t iOffset = inumber % INODES_PER_BLOCK;
+
+    // read in inode block
+    fs_block block;
+    if (disk_read(thedisk, iBlock, block.data) == (-1)){
+        return 0;
+    }
+
+    //node = block.inodes[iOffset];
+    // copy to node pointer the inode info
+    memcpy(node, &block.inode[iOffset], sizeof(fs_inode));
+    return 1;
+}
+
+bool fs_save_inode(size_t inumber, Inode *node){
+
+    // find blk. offset val
+    ssize_t iBlock = (ssize_t) ((inumber + INODES_PER_BLOCK - 1)/ INODES_PER_BLOCK);
+    
+    if (iBlock == 0){
+        iBlock = 1;
+    }
+    ssize_t iOffset = inumber % INODES_PER_BLOCK;
+
+    // read in block
+    fs_block block;
+    if (disk_read(thedisk, iBlock, block.data) == (-1)){
+        return 0;
+    }
+
+    // cpy to blk inode info
+    memcpy(&block.inode[iOffset], node, sizeof(fs_inode));
+
+    // write inode info to disk
+    if (disk_write(thedisk, iBlock, block.data) == (-1)){
+        return 0;
+    }
+
+    return 1;
+}
+
+// ************************************
+
 
 int fs_format()
 {
-	return 0;
+	// check if it already has been mounted
+  if (thedisk) return 0;
+
+  // format the superblock 
+  fs_block block = {{0}};
+  block.super.magic_number = FS_MAGIC;
+  block.super.blocks = thedisk.nblocks;
+  block.super.ninodeblocks = (uint32_t) ((thedisk.nblocks + 9) / 10);
+  block.super.ninodes = block.super.ninodeblocks * INODES_PER_BLOCK;
+
+	// set block data to superblock
+  memcpy(&block.data, &block.super, sizeof(fs_superblock));
+    
+  // Write super block before clearing it
+  if (disk_write(thedisk, 0, block.data) == (-1)){
+      return 0;
+  }
+  // set block data to 0
+  memset(block.data, 0, BLOCK_SIZE); // set block data to 0
+
+  // loop through all other blocks and write the 0 data to them
+  for (int i=1; i < thedisk.nblocks; i++){
+      if (disk_write(disk, i, block.data) == (-1)){
+         return 0;
+      }
+  }
+
+	return 1;
 }
 
 void fs_debug()
@@ -65,7 +151,7 @@ void fs_debug()
 						m++;
 						k++;
 					}
-					printf("\n");
+					// printf("\n");
 				}
 
 			}
@@ -74,31 +160,32 @@ void fs_debug()
 }
 
 int fs_mount()
-{
-	return 0;
+    return 0;
 }
 
-int fs_create()
-{
+
+int fs_create() {
 	return 0;
 }
 
 int fs_delete( int inumber )
 {
-	return 0;
+
+    return 0;
 }
 
 int fs_getsize( int inumber )
 {
-	return 0;
+  return 0;
 }
 
 int fs_read( int inumber, char *data, int length, int offset )
 {
-	return 0;
+    return 0;
 }
 
 int fs_write( int inumber, const char *data, int length, int offset )
 {
-	return 0;
+      
+    return 0; 
 }
